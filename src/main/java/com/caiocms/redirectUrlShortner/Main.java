c091bf7692b397b4b1a5a5fe3e93d458f1e74966
Caio Santos <cs1@cloudplusplus.nl>
2024-11-24 11:53:17 -0300

    Aula 3

---
+++ src/main/java/com/caiocms/redirectUrlShortner/Main.java
@@ -0,0 +1,67 @@
+package com.caiocms.redirectUrlShortner;
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+    private final S3Client s3Client = S3Client.builder().build();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        String pathParameters = (String) input.get("rawPath");
+        String shortUrlCode = pathParameters.replace("/", "");
+
+        if (shortUrlCode == null || shortUrlCode.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required.");
+        }
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("url-shortener-storage-lambda-caio-alura")
+                .key(shortUrlCode + ".json")
+                .build();
+
+        InputStream s3ObjectStream;
+
+        try {
+            s3ObjectStream = s3Client.getObject(getObjectRequest);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error fetching URL data from S3: " + exception.getMessage(), exception);
+        }
+
+        UrlData urlData;
+        try {
+            urlData = objectMapper.readValue(s3ObjectStream, UrlData.class);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error deserializing URL data: " + exception.getMessage(), exception);
+        }
+
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000;
+
+        Map<String, Object> response = new HashMap<>();
+        if (currentTimeInSeconds > urlData.getExpirationTime()) {
+            response.put("statusCode", 410);
+            response.put("body", "This URL has expired.");
+            return response;
+        }
+
+        response.put("statusCode", 302);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", urlData.getOriginalUrl());
+        response.put("headers", headers);
+
+        LambdaLogger logger = context.getLogger();
+        logger.log("DEBUG: " + urlData.getOriginalUrl());
+
+        return response;
+    }
+}
